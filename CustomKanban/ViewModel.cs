24254322cfd466302b6261fe3f77cf8b22991e1d
Malcolm Jack <inquisitorjax@gmail.com>
2016-12-01 17:29:28 +0200

    formatting

--- CustomKanban/ViewModel.cs
+++ CustomKanban/ViewModel.cs
@@ -1,13 +1,3 @@
-#region Copyright Syncfusion Inc. 2001 - 2016
-
-// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
-// Use of this code is subject to the terms of our license.
-// A copy of the current license can be obtained at any time by e-mailing
-// licensing@syncfusion.com. Any infringement will be prosecuted under
-// applicable laws.
-
-#endregion Copyright Syncfusion Inc. 2001 - 2016
-
 using Prism.Commands;
 using System;
 using System.Collections.Generic;
@@ -29,9 +19,9 @@ namespace CustomKanban
             Cards.Add(new CustomModel
             {
                 Id = Guid.NewGuid().ToString(),
-                Title = "The Flash",
+                Title = "Superman",
                 Category = "Beginner",
-                Description = "The fastest man alive",
+                Description = "The Man of Steel",
                 Picture = null,
                 Status = (int)CustomCardStatus.Beginner
             });
